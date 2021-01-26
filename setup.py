7fcfcf94af8631e23f17bb9929e3319d60f2267a
dongree <dongwoo0307@naver.com>
2021-01-26 11:13:04 +0900

    basic setting

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='corona_lib',
+      version='0.1',
+      description='corona pip package',
+      url='https://github.com/dongree/corona19_pip_package.git',
+      author='dongree',
+      author_email='dongwoo0307@naver.com',
+      license='dongree',
+      packages=['corona_lib'],
+      zip_safe=False,
+      install_requires=[
+
+      ]
+      )
